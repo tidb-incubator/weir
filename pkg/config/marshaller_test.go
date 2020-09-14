3a37f00a91407a53911a511a3abc7dcd4ee8fdd1
eastfisher <eastfisher72@gmail.com>
2020-09-14 19:12:34 +0800

    add proxy config and proxy server

---
+++ pkg/config/marshaller_test.go
@@ -0,0 +1,85 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testNamespaceConfig = Namespace{
+	Version:   "v1",
+	Namespace: "test_ns",
+	Frontend: FrontendNamespace{
+		AllowedDBs:  []string{"db0", "db1"},
+		SlowSQLTime: 10,
+		DeniedSQLs:  []string{"known", "unknown"},
+		DeniedIPs:   []string{"127.0.0.0", "128.0.0.0"},
+		IdleTimeout: 10,
+		Users: []FrontendUserInfo{
+			{Username: "user0", Password: "pwd0"},
+			{Username: "user1", Password: "pwd1"},
+		},
+	},
+	Backend: BackendNamespace{
+		Username:     "user0",
+		Password:     "pwd0",
+		Instances:    []string{"127.0.0.1:4000", "127.0.0.1:4001"},
+		SelectorType: "random",
+		PoolSize:     1,
+		IdleTimeout:  20,
+	},
+}
+
+var testProxyConfig = Proxy{
+	Version: "v1",
+	ProxyServer: ProxyServer{
+		Addr:           "0.0.0.0:4000",
+		MaxConnections: 1,
+	},
+	AdminServer: AdminServer{
+		Addr:            "0.0.0.0:4001",
+		EnableBasicAuth: false,
+		User:            "user",
+		Password:        "pwd",
+	},
+	Log: Log{
+		Level:  "info",
+		Format: "console",
+		LogFile: LogFile{
+			Filename:   ".",
+			MaxSize:    10,
+			MaxDays:    1,
+			MaxBackups: 1,
+		},
+	},
+	Registry: Registry{
+		Enable: false,
+		Type:   "etcd",
+		Addrs:  []string{"127.0.0.1:4000", "127.0.0.1:4001"},
+	},
+	ConfigCenter: ConfigCenter{
+		Type: "file",
+		ConfigFile: ConfigFile{
+			Path: ".",
+		},
+	},
+	Performance: Performance{
+		TCPKeepAlive: true,
+	},
+}
+
+func TestNamespaceConfigEncodeAndDecode(t *testing.T) {
+	data, err := MarshalNamespaceConfig(&testNamespaceConfig)
+	assert.NoError(t, err)
+	cfg, err := UnmarshalNamespaceConfig(data)
+	assert.NoError(t, err)
+	assert.Equal(t, testNamespaceConfig, *cfg)
+}
+
+func TestProxyConfigEncodeAndDecode(t *testing.T) {
+	data, err := MarshalProxyConfig(&testProxyConfig)
+	assert.NoError(t, err)
+	cfg, err := UnmarshalProxyConfig(data)
+	assert.NoError(t, err)
+	assert.Equal(t, testProxyConfig, *cfg)
+}
---
+++ pkg/proxy/server/server_util.go
@@ -0,0 +1,41 @@
+// Copyright 2015 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"sync"
+
+	"github.com/pingcap/tidb/sessionctx/variable"
+	"github.com/pingcap/tidb/util/logutil"
+	"github.com/pingcap/tidb/util/timeutil"
+	"go.uber.org/zap"
+)
+
+// setSysTimeZoneOnce is used for parallel run tests. When several servers are running,
+// only the first will actually do setSystemTimeZoneVariable, thus we can avoid data race.
+var setSysTimeZoneOnce = &sync.Once{}
+
+func setSystemTimeZoneVariable() {
+	setSysTimeZoneOnce.Do(func() {
+		tz, err := timeutil.GetSystemTZ()
+		if err != nil {
+			logutil.BgLogger().Error(
+				"Error getting SystemTZ, use default value instead",
+				zap.Error(err),
+				zap.String("default system_time_zone", variable.SysVars["system_time_zone"].Value))
+			return
+		}
+		variable.SysVars["system_time_zone"].Value = tz
+	})
+}
