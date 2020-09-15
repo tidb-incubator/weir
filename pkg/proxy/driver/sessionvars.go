45678ccc56cd4c5030b130d1e64de918dab788d4
eastfisher <eastfisher72@gmail.com>
2020-09-15 19:12:44 +0800

    add SessionVarsWrapper

---
+++ pkg/proxy/driver/sessionvars.go
@@ -0,0 +1,71 @@
+package driver
+
+import (
+	"sync/atomic"
+
+	"github.com/pingcap/tidb/sessionctx/variable"
+)
+
+type SessionVarsWrapper struct {
+	sessionVars *variable.SessionVars
+}
+
+func NewSessionVarsWrapper(sessionVars *variable.SessionVars) *SessionVarsWrapper {
+	return &SessionVarsWrapper{sessionVars: sessionVars}
+}
+
+func (s *SessionVarsWrapper) SessionVars() *variable.SessionVars {
+	return s.sessionVars
+}
+
+func (s *SessionVarsWrapper) GetSystemVar(name string) (string, bool) {
+	return s.sessionVars.GetSystemVar(name)
+}
+
+func (s *SessionVarsWrapper) SetSystemVar(name string, val string) error {
+	return s.sessionVars.SetSystemVar(name, val)
+}
+
+func (s *SessionVarsWrapper) Status() uint16 {
+	return s.sessionVars.Status
+}
+
+func (s *SessionVarsWrapper) SetStatusFlag(flag uint16, on bool) {
+	s.sessionVars.SetStatusFlag(flag, on)
+}
+
+func (s *SessionVarsWrapper) GetCharsetInfo() (charset, collation string) {
+	return s.sessionVars.GetCharsetInfo()
+}
+
+func (s *SessionVarsWrapper) AffectedRows() uint64 {
+	return s.sessionVars.StmtCtx.AffectedRows()
+}
+
+func (s *SessionVarsWrapper) LastInsertID() uint64 {
+	return s.sessionVars.StmtCtx.LastInsertID
+}
+
+func (s *SessionVarsWrapper) SetLastInsertID(id uint64) {
+	s.sessionVars.StmtCtx.LastInsertID = id
+}
+
+func (s *SessionVarsWrapper) GetMessage() string {
+	return s.sessionVars.StmtCtx.GetMessage()
+}
+
+func (s *SessionVarsWrapper) SetMessage(msg string) {
+	s.sessionVars.StmtCtx.SetMessage(msg)
+}
+
+func (s *SessionVarsWrapper) GetClientCapability() uint32 {
+	return s.sessionVars.ClientCapability
+}
+
+func (s *SessionVarsWrapper) SetClientCapability(capability uint32) {
+	s.sessionVars.ClientCapability = capability
+}
+
+func (s *SessionVarsWrapper) SetCommandValue(command byte) {
+	atomic.StoreUint32(&s.sessionVars.CommandValue, uint32(command))
+}
