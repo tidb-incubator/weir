077faba735831563a6505af9f4a3622912e7933c
eastfisher <eastfisher72@gmail.com>
2020-09-23 17:41:33 +0800

    first commit proxy driver (#8)
    * first commit proxy driver
    * remove Auth() in Namespace
    * update go.mod

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/pingcap/failpoint v0.0.0-20200702092429-9f69995143ce
 	github.com/pingcap/parser v0.0.0-20200803072748-fdf66528323d
 	github.com/pingcap/tidb v1.1.0-beta.0.20200826081922-9c1c21270001
+	github.com/siddontang/go-mysql v1.1.0
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/zap v1.15.0
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73
---
+++ pkg/proxy/driver/resultset.go
@@ -0,0 +1,117 @@
+package driver
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+
+	"github.com/pingcap-incubator/weir/pkg/proxy/server"
+	"github.com/pingcap/tidb/types"
+	"github.com/pingcap/tidb/util/chunk"
+	"github.com/pingcap/tidb/util/hack"
+	"github.com/siddontang/go-mysql/mysql"
+)
+
+type weirResultSet struct {
+	result      *mysql.Result
+	columnInfos []*server.ColumnInfo
+	closed      int32
+	readed      bool
+}
+
+func wrapMySQLResult(result *mysql.Result) *weirResultSet {
+	resultSet := &weirResultSet{
+		result: result,
+	}
+	columnInfos := convertFieldsToColumnInfos(result.Fields)
+	resultSet.columnInfos = columnInfos
+	return resultSet
+}
+
+func convertFieldsToColumnInfos(fields []*mysql.Field) []*server.ColumnInfo {
+	var columnInfos []*server.ColumnInfo
+	for _, field := range fields {
+		columnInfo := &server.ColumnInfo{
+			Schema:             string(hack.String(field.Schema)),
+			Table:              string(hack.String(field.Table)),
+			OrgTable:           string(hack.String(field.OrgTable)),
+			Name:               string(hack.String(field.Name)),
+			OrgName:            string(hack.String(field.OrgName)),
+			ColumnLength:       field.ColumnLength,
+			Charset:            field.Charset,
+			Flag:               field.Flag,
+			Decimal:            field.Decimal,
+			Type:               field.Type,
+			DefaultValueLength: field.DefaultValueLength,
+			DefaultValue:       field.DefaultValue,
+		}
+		columnInfos = append(columnInfos, columnInfo)
+	}
+	return columnInfos
+}
+
+func convertFieldTypes(fields []*mysql.Field) []*types.FieldType {
+	var ret []*types.FieldType
+	for _, f := range fields {
+		ft := types.NewFieldType(f.Type)
+		ft.Flag = uint(f.Flag)
+		ret = append(ret, ft)
+	}
+	return ret
+}
+
+func writeResultSetDataToTrunk(r *mysql.Resultset, c *chunk.Chunk) {
+	for _, rowValue := range r.Values {
+		for colIdx, colValue := range rowValue {
+			switch colValue.Type {
+			case mysql.FieldValueTypeNull:
+				c.Column(colIdx).AppendNull()
+			case mysql.FieldValueTypeUnsigned:
+				c.Column(colIdx).AppendUint64(colValue.AsUint64())
+			case mysql.FieldValueTypeSigned:
+				c.Column(colIdx).AppendInt64(colValue.AsInt64())
+			case mysql.FieldValueTypeFloat:
+				c.Column(colIdx).AppendFloat64(colValue.AsFloat64())
+			case mysql.FieldValueTypeString:
+				c.Column(colIdx).AppendBytes(colValue.AsString())
+			default:
+				panic(fmt.Errorf("invalid col value type: %v", colValue.Type))
+			}
+		}
+	}
+}
+
+func (w *weirResultSet) Columns() []*server.ColumnInfo {
+	return w.columnInfos
+}
+
+func (w *weirResultSet) NewChunk() *chunk.Chunk {
+	columns := convertFieldTypes(w.result.Fields)
+	rowCount := len(w.result.RowDatas)
+	c := chunk.NewChunkWithCapacity(columns, rowCount)
+	writeResultSetDataToTrunk(w.result.Resultset, c)
+	return c
+}
+
+func (w *weirResultSet) Next(ctx context.Context, c *chunk.Chunk) error {
+	// all the data has been converted and set into chunk when calling NewChunk(),
+	// so we need to do nothing here
+	if w.readed {
+		c.Reset()
+	}
+	w.readed = true
+	return nil
+}
+
+func (*weirResultSet) StoreFetchedRows(rows []chunk.Row) {
+	panic("implement me")
+}
+
+func (*weirResultSet) GetFetchedRows() []chunk.Row {
+	panic("implement me")
+}
+
+func (w *weirResultSet) Close() error {
+	atomic.StoreInt32(&w.closed, 1)
+	return nil
+}
