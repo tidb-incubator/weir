8221f08b74824e753a0af6fb7c24cc7f33718a48
eastfisher <eastfisher72@gmail.com>
2020-10-12 21:33:38 +0800

    delete unused code in namespace

--- pkg/proxy/namespace/errcode.go
+++ pkg/proxy/namespace/errcode.go
@@ -7,9 +7,4 @@ import (
 var (
 	ErrDuplicatedUser      = errors.New("duplicated user")
 	ErrInvalidSelectorType = errors.New("invalid selector type")
-	ErrNamespaceNotFound   = errors.New("namespace not found")
-	ErrDuplicatedNamespace = errors.New("duplicated namespace")
-	ErrInitBackend         = errors.New("init backend error")
-	ErrInitFrontend        = errors.New("init frontend error")
-	ErrInitUsers           = errors.New("init users error")
 )
