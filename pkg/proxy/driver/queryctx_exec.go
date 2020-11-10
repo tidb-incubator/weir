8177a356e36cc3fa7cb1f6323c3a834b0fab42d0
eastfisher <eastfisher72@gmail.com>
2020-11-10 15:33:08 +0800

    fix affectrows (#25)
    * fix affect rows error
    * remove unused function

--- pkg/proxy/driver/queryctx_exec.go
+++ pkg/proxy/driver/queryctx_exec.go
@@ -103,28 +103,10 @@ func (q *QueryCtxImpl) executeInBackend(ctx context.Context, sql string, stmtNod
 		return nil, err
 	}
 
-	if result.Resultset == nil {
-		q.sessionVars.SetAffectRows(result.AffectedRows)
-		q.sessionVars.SetLastInsertID(result.InsertId)
-		return nil, nil
-	}
-
-	return result, nil
-}
-
-func (q *QueryCtxImpl) executeInBackendConn(ctx context.Context, conn PooledBackendConn, db string, sql string, stmtNode ast.StmtNode) (*gomysql.Result, error) {
-	if err := conn.UseDB(db); err != nil {
-		return nil, err
-	}
-
-	result, err := conn.Execute(sql)
-	if err != nil {
-		return nil, err
-	}
+	q.sessionVars.SetAffectRows(result.AffectedRows)
+	q.sessionVars.SetLastInsertID(result.InsertId)
 
 	if result.Resultset == nil {
-		q.sessionVars.SetAffectRows(result.AffectedRows)
-		q.sessionVars.SetLastInsertID(result.InsertId)
 		return nil, nil
 	}
 
