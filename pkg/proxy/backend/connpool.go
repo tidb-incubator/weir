301fde3a6e1a4953aa9a68395285419d6f8312f7
eastfisher <eastfisher72@gmail.com>
2020-12-21 19:42:56 +0800

    if set variable value equals, continue

--- pkg/proxy/backend/connpool.go
+++ pkg/proxy/backend/connpool.go
@@ -200,7 +200,10 @@ func getRestoreSQLFromStmt(stmt *ast.SetStmt) (string, error) {
 // get variables to set and variables to set default
 func getDiffVariableList(frontend, current map[string]*ast.VariableAssignment) ([]*ast.VariableAssignment, []*ast.VariableAssignment) {
 	var toSet, toRemove []*ast.VariableAssignment
-	for _, v := range frontend {
+	for k, v := range frontend {
+		if currentValue, ok := current[k]; ok && isAstEquals(currentValue, v) {
+			continue
+		}
 		toSet = append(toSet, v)
 	}
 	for k, v := range current {
@@ -209,4 +212,15 @@ func getDiffVariableList(frontend, current map[string]*ast.VariableAssignment) (
 		}
 	}
 	return toSet, toRemove
+}
+
+func isAstEquals(a, b ast.Node) bool {
+	sba := strings.Builder{}
+	ctx1 := format.NewRestoreCtx(RestoreSetVariableFlags, &sba)
+	a.Restore(ctx1)
+
+	sbb := strings.Builder{}
+	ctx2 := format.NewRestoreCtx(RestoreSetVariableFlags, &sbb)
+	b.Restore(ctx2)
+	return sba.String() == sbb.String()
 }
