0890b76889a7110b5db654836dfbaff65e317031
eastfisher <eastfisher72@gmail.com>
2021-03-23 21:32:09 +0800

    fix unit test

--- pkg/util/errors/errors_test.go
+++ pkg/util/errors/errors_test.go
@@ -1,14 +1,22 @@
 package errors
 
 import (
+	stderrors "errors"
 	"testing"
 
 	"github.com/pingcap/errors"
 	"github.com/siddontang/go-mysql/mysql"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIs(t *testing.T) {
 	badConn := mysql.ErrBadConn
-	err := errors.AddStack(badConn)
-	t.Log(Is(err, badConn))
+	err := errors.Wrapf(badConn, "same error type")
+	assert.True(t, Is(err, badConn))
+}
+
+func TestStdIs(t *testing.T) {
+	badConn := mysql.ErrBadConn
+	err := errors.Wrapf(badConn, "another error type")
+	assert.False(t, stderrors.Is(err, badConn))
 }
